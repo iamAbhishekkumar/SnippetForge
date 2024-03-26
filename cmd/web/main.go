c3f59e971a04ed5c95de46b782386887b1130a4f
iamAbhishekkumar <abhisonuk123@gmail.com>
2024-03-26 21:22:33 +0530

    added static files serving and basic css and vanilla js

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,9 +6,18 @@ import (
 )
 
 func main() {
+	mux := http.NewServeMux()
+
+	// Create a file server which serves files out of the "./ui/static" directory.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	// Register the two new handler functions and corresponding URL patterns with
 	// the servemux, in exactly the same way that we did before.
-	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", viewSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
