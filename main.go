3d2874da06afda4a70ba8c147e6db60e9d1161d1
iamAbhishekkumar <abhisonuk123@gmail.com>
2024-03-26 20:58:41 +0530

    added how to query string

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -10,12 +12,32 @@ func home(w http.ResponseWriter, r *http.Request) {
 } // Add a snippetView handler function.
 
 func viewSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 // Add a createSnippet handler function.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a new snippet..."))
+	if r.Method != "POST" {
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body.
+
+		/*
+			w.WriteHeader(405)
+			w.Write([]byte("Method Not Allowed"))
+			This can be also written as :
+		*/
+		// w.Header()["Date"] = nil // for suppressing any header
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 }
 
 func main() {
